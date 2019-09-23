83129d3befb2b2b5ad5cf27f41579b4ab6592223
Matthew Dang <matthewkdang@gmail.com>
2019-09-23 09:32:41 -0700

    added to questions.js

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "How do we print to console?",
+        choices: ["console.log ();", "consolelog();", "console{};", "log.console();"],
+        answer: "console.log ();"
+    },
+    
+    { 
+        title: "Any type of element can be stored in an array.",
+        choices: ["true", "false"],
+        answer: "true"
+    },
+
+    {   
+        title: "What is a boolean?", 
+        choices: ["a value that represents true or false.", "a shared place to store project files and revision history.", "a list of elements.", "a password"],
+        answer: "a value that represents true or false"
+    },
+
+    {  
+        title: "Items stored in local storage have no expiration time.",
+        choices: ["true", "false"],
+        answer: "true"
+    }, 
+
+    {
+        title: "Once an element is stored in an array it cannot be removed.",
+        choices: ["true", "false"],
+        answer: "false"
+    },
+]; 
