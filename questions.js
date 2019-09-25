0ef9c5c79c79474ea27c3f5e5d6086427b225060
Matthew Dang <matthewkdang@gmail.com>
2019-09-24 22:08:11 -0700

    added for loops and function goNext to advance through questions. trying to work on timer & score functionality

--- questions.js
+++ questions.js
@@ -1,42 +1,43 @@
 var questions = [
     {
-        title: "How do we print to console?",
+        title: "1)  How do we print to console?",
         choices: ["console.log ();", "consolelog();", "console{};", "log.console();"],
         answer: "console.log ();"
     },
     
     { 
-        title: "Any type of element can be stored in an array.",
+        title: "2)  Any type of element can be stored in an array.",
         choices: ["true", "false"],
         answer: "true"
     },
 
     {   
-        title: "What is a boolean?", 
+        title: "3)  What is a boolean?", 
         choices: ["a value that represents true or false.", "a shared place to store project files and revision history.", "a list of elements.", "a password"],
         answer: "a value that represents true or false"
     },
 
     {  
-        title: "Items stored in local storage have no expiration time.",
+        title: "4)  Items stored in local storage have no expiration time.",
         choices: ["true", "false"],
         answer: "true"
     }, 
 
     {
-        title: "Once an element is stored in an array it cannot be removed.",
+        title: "5)  Once an element is stored in an array it cannot be removed.",
         choices: ["true", "false"],
         answer: "false"
     },
 ]; 
 
-var alotted = 75000;
+var allotted = 75000;
 var timer;
 var score = 0;
 
 function buildQuiz () {
     setTimeout(function(){
         alert("Hello"); }, 75000);
+    
     var quiz = [ ];
     var answers = [ ];
     for (i = 0; i < questions.length; i++)
@@ -45,7 +46,7 @@ function buildQuiz () {
         for (j = 0; j < questions[i].choices.length; j ++) 
         
         {
-            answers.push('<input type="radio">' + questions[i].choices[j] + '<input>');
+            answers.push('<label><input type="radio" name= "grouping" onchange="answerQuestion('+i+', \''+questions[i].choices[j]+'\')">' + questions[i].choices[j] + '</label><br>');
         }
             quiz.push(
                 '<div class="question">' + questions[i].title + '</div>' + '<div class="answers">' + answers.join(' ') + '</div>'
@@ -56,15 +57,34 @@ function buildQuiz () {
     document.getElementById("quiz").innerHTML = quiz.join("");
 }
 
- // Start Quiz -- onclick hide elements and show questions
+function goNext (qnum) {
+    questionTimer = setTimeout("calculateScore()", 15000);
+    var quiz = [];
+    var answers = [];
+    document.getElementById("quiz").innerHTML = " ";
+    for(j = 0; j < questions[qnum].choices.length;j++)
+    {
+        answers.push('<label><input type="radio" name= "grouping" onchange="answerQuestion('+qnum+', \''+questions[qnum].choices[j]+'\')">' + questions[qnum].choices[j] + '</label><br>');
+       }
+       quiz.push(
+        '<div class="question">' + questions[i].title + '</div>' + '<div class="answers">' + answers.join(' ') + '</div>'
+        );
+        document.getElementById("quiz").innerHTML = quiz.join("");
+}
 
-    // timeinterval decrease 
+function answerQuestion(qnum, choice) {
+    console.log(qnum);
+    console.log(choice);
+    if (questions[qnum].answer !== choice) {
 
-    // store and increment scores in localstorage
+    }
+    qnum = qnum + 1;
+    goNext(qnum);
+}
 
-    // endgame on timer 0  
-
-    // create an event listener on button
+// function calculateScore( ) {
+//     console.log(calculateScore);
+// }
 
     document.querySelector("button").addEventListener("click", function(){
         let h1Tag = document.querySelector("h1");
